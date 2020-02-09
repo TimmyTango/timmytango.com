2e013ccdb9d2ccabfb8d21a608fe1670fb1c9a57
Timothy Hamilton <TimH@crossbrowsertesting.com>
2020-02-08 23:04:44 -0600

    font and icon optimizations

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,12 @@
+import { library, dom } from '@fortawesome/fontawesome-svg-core';
+import { faBars } from '@fortawesome/free-solid-svg-icons/faBars';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons/faEnvelope';
+import { faGithub } from '@fortawesome/free-brands-svg-icons/faGithub';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons/faLinkedin'
+
+library.add(faBars, faEnvelope, faGithub, faLinkedin);
+dom.watch();
+
 const topNav = document.querySelector('#top-nav');
 const menuButton = topNav.querySelector('i');
 const menu = topNav.querySelector('ul');
