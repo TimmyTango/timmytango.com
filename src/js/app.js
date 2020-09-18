d36bbf73db713e0bd6a4e79393ceede53e0f4c25
Tim Hamilton <timothyahamilton@gmail.com>
2020-09-18 10:27:18 -0500

    WIP update to site

--- src/js/app.js
+++ src/js/app.js
@@ -5,37 +5,6 @@ import {faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 library.add(faBars, faEnvelope, faGithub, faLinkedin);
 dom.watch();
 
-const topNav = document.querySelector('#top-nav');
-const menuButton = topNav.querySelector('.bars');
-const menu = topNav.querySelector('ul');
-const sectionLinks = menu.querySelectorAll('li');
-
-for(let link of sectionLinks) {
-    link.addEventListener('click', event => {
-        document.querySelector(`section#${link.innerText.toLowerCase()} span.anchor`).scrollIntoView({behavior: "smooth"});
-    })
-}
-
-menuButton.addEventListener('click', event => {
-    event.stopPropagation();
-
-    if (menu.classList.contains('active')) {
-        closeMenu();
-    } else {
-        openMenu();
-    }
-})
-
-function openMenu() {
-    menu.classList.add('active');
-    document.addEventListener('click', closeMenu);
-}
-
-function closeMenu() {
-    menu.classList.remove('active');
-    document.removeEventListener('click', closeMenu);
-}
-
 function toggleImageExpansion(event) {
     if (window.innerWidth > 576) {
         const img = event.target;
